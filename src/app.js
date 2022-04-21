8d706fc9f0172696d906473cc36f27acda8d78f8
Frederick Lopez <mtester010@gmail.com>
2022-04-21 14:51:15 -0500

    Setup the questions for the game

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ function runInstruction() {
   const autoType = document.getElementById('type-instruction');
   let playerName = document.querySelector('.playerName').value;
   console.log(playerName);
-  var buildWeb = `Hi ${playerName}, You've been lucky to play my quiz game. In order to win the prize you need to reach the gold tier and answer all of the questions. You only have 3 lives, so use it wisely. Each questions you need to answer within the time limit. You need to choose the answer correctly or else your life will be deducted. Enjoy!`;
+  var buildWeb = `Hi ${playerName}, You've been lucky to play my quiz game. In order for you to join my forces you need to reach the square tier and answer all of the questions correctly. You only have 3 lives, so use it wisely. You need to choose the correct anwer or else you will fail.`;
   var index = 0;
 
   window.next_letter = function () {
@@ -28,9 +28,32 @@ function runInstruction() {
       autoType.textContent = buildWeb.substr(0, index++);
       setTimeout("next_letter()", 60);
     }
+
+    if (index === buildWeb.length) {
+      let startBtn = document.querySelector('.initiate-game')
+      startBtn.style.display = 'block';
+    }
   }
 
   next_letter();
 }
 
 
+const easyQuestions = fetch('https://opentdb.com/api.php?amount=5&difficulty=easy')
+  .then((result) => result.json())
+  .then((data) => data.results)
+  .then((data) => console.log(data))
+
+const medQuestions = fetch('https://opentdb.com/api.php?amount=5&difficulty=medium')
+  .then((result) => result.json())
+  .then((data) => data.results)
+  .then((data) => console.log(data))
+
+const hardQuestions = fetch('https://opentdb.com/api.php?amount=5&difficulty=hard')
+  .then((result) => result.json())
+  .then((data) => data.results)
+
+
+Promise.all([easyQuestions, medQuestions, hardQuestions]).then((values) => {
+  console.log(values);
+});
